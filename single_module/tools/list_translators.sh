58e16033666bfd644915c0ec76efc106efda1218
Mark McLoughlin <mark@skynet.ie>
2003-06-12 16:49:11 +0000

    fix buglet where the .po filename is printed instead of just the language
    2003-06-12  Mark McLoughlin  <mark@skynet.ie>
    
            * single_module/tools/list_translators.sh: fix buglet
            where the .po filename is printed instead of just the
            language code.

--- single_module/tools/list_translators.sh
+++ single_module/tools/list_translators.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 # usage: tranlators.sh LAST_RELEASE_TAG
+#
+# Note: must be run from the module's toplevel
+# directory.
 
 runsocks cvs diff -r $1 po/ChangeLog | \
   (awk  '/\+.*([a-z][a-zA-Z@]*\.po)/ { print gensub (":", " ", "g", $3); }' | \
     while read file; do
-  echo $(grep "Last-Translator" po/$file | sed -e 's/"Last-Translator:  *\(.*\)  *<.*/\1/') "($file)"
+  echo $(grep "Last-Translator" po/$file | sed -e 's/"Last-Translator:  *\(.*\)  *<.*/\1/') "(${file%%.po})"
 done) | sort | uniq
