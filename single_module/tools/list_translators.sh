b05e2ff616000365c1a9ec460dd24f398c24efda
Mark McLoughlin <mark@skynet.ie>
2003-06-14 13:49:36 +0000

    kill runsocks foo.
    2003-06-14  Mark McLoughlin  <mark@skynet.ie>
    
            * single_module/tools/list_translators.sh: kill runsocks
            foo.

--- single_module/tools/list_translators.sh
+++ single_module/tools/list_translators.sh
@@ -5,7 +5,7 @@
 # Note: must be run from the module's toplevel
 # directory.
 
-runsocks cvs diff -r $1 po/ChangeLog | \
+cvs diff -r $1 po/ChangeLog | \
   (awk  '/\+.*([a-z][a-zA-Z@]*\.po)/ { print gensub (":", " ", "g", $3); }' | \
     while read file; do
   echo $(grep "Last-Translator" po/$file | sed -e 's/"Last-Translator:  *\(.*\)  *<.*/\1/') "(${file%%.po})"
