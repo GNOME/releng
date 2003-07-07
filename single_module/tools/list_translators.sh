47ceb2925ed1a55a451ff74ac64045c7275026c1
Mark McLoughlin <mark@skynet.ie>
2003-07-07 15:01:09 +0000

    fix a couple of things.
    2003-07-07  Mark McLoughlin  <mark@skynet.ie>
    
            * single_module/tools/list_translators.sh: fix a couple of things.

--- single_module/tools/list_translators.sh
+++ single_module/tools/list_translators.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 # usage: tranlators.sh LAST_RELEASE_TAG
-#
-# Note: must be run from the module's toplevel
-# directory.
 
 cvs diff -r $1 po/ChangeLog | \
-  (awk  '/\+.*([a-z][a-zA-Z@]*\.po)/ { print gensub (":", " ", "g", $3); }' | \
+  (awk  '/\+.*[a-z][a-zA-Z@]*\.po/ { print gensub ("[:,]", " ", "g", $3); }' | \
     while read file; do
   echo $(grep "Last-Translator" po/$file | sed -e 's/"Last-Translator:  *\(.*\)  *<.*/\1/') "(${file%%.po})"
 done) | sort | uniq
