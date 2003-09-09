d5caaef48f43fbde7c82529ec32584df85c3c4d3
Mark McLoughlin <mark@skynet.ie>
2003-09-09 14:44:14 +0000

    play with the voodoo a bit.
    2003-09-09  Mark McLoughlin  <mark@skynet.ie>
    
            * single_module/tools/list_translators.sh: play with the voodoo
            a bit.

--- single_module/tools/list_translators.sh
+++ single_module/tools/list_translators.sh
@@ -3,7 +3,8 @@
 # usage: tranlators.sh LAST_RELEASE_TAG
 
 cvs diff -r $1 po/ChangeLog | \
-  (awk  '/\+.*[a-z][a-zA-Z@]*\.po/ { print gensub ("[:,]", " ", "g", $3); }' | \
+  (awk  '/\+.*[a-z][a-zA-Z@]*\.po/ { print gensub ("[:,]", "\n", "g", $3); }' | \
     while read file; do
+  if [ -z "$file" ]; then continue; fi
   echo $(grep "Last-Translator" po/$file | sed -e 's/"Last-Translator:  *\(.*\)  *<.*/\1/') "(${file%%.po})"
 done) | sort | uniq
