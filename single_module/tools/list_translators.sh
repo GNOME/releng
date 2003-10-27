f0ce5e49183efd1f93f232a8d5af67b92651845b
Kjartan Maraas <kmaraas@gnome.org>
2003-10-27 14:19:45 +0000

    Add -u to the cvs command so that the script actually works.
    2003-10-27  Kjartan Maraas  <kmaraas@gnome.org>
    
    	* single_module/tools/list_translators.sh: Add -u to the cvs command
    	so that the script actually works.

--- single_module/tools/list_translators.sh
+++ single_module/tools/list_translators.sh
@@ -7,7 +7,7 @@ if [ ! $# -eq 1 ] ; then
 	exit 1;
 fi
 
-cvs diff -r $1 po/ChangeLog | \
+cvs diff -u -r $1 po/ChangeLog | \
   (awk  '/\+.*[a-z][a-zA-Z@_]*\.po/ { print gensub ("[:,]", "\n", "g", $3); }' | \
     while read file; do
   if [ -z "$file" ]; then continue; fi
