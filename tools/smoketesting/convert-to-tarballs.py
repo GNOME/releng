01c4cb5d74385d319e90dc215e181960ac431268
Vincent Untz <vuntz@gnome.org>
2009-09-02 06:43:03 +0200

    [smoketesting] Add LGPLv2.1+ license to convert-to-tarballs.py
    This was discussed with Elijah and Olav. It's mainly needed because I've
    reused part of the code elsewhere.

--- tools/smoketesting/convert-to-tarballs.py
+++ tools/smoketesting/convert-to-tarballs.py
@@ -1,5 +1,24 @@
 #!/usr/bin/python
 
+# Copyright (c) 2005-2008, Elijah Newren
+# Copyright (c) 2007-2009, Olav Vitters
+# Copyright (c) 2006-2009, Vincent Untz
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+# USA
+
 # Example of running:
 #   $ ./convert-to-tarballs.py -t ~/src/tarball-gnome2/pkgs -v 2.11.91 \
 #       ~/src/jhbuild/modulesets/gnome-2.12.modules
