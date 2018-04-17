376c32d6a77368cba39093758329172214d5ce36
Michael Catanzaro <mcatanzaro@igalia.com>
2018-04-17 14:09:10 -0500

    convert-to-tarballs: also delete submodules

--- tools/smoketesting/convert-to-tarballs.py
+++ tools/smoketesting/convert-to-tarballs.py
@@ -658,6 +658,7 @@ class ConvertToTarballs:
         element['sources'][0]['kind'] = 'tar'
         element['sources'][0]['url'] = location
         element['sources'][0]['ref'] = sha
+        del element['sources'][0]['submodules']
         del element['sources'][0]['track']
 
         # Dump it now
