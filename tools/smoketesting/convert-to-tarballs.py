GNOME/releng

commit 62c85310841e1cab8d16b1e71fdef401954f35ab

Port convert-to-tarballs to HTMLParser
SGMLParser is deprecated since python 2.6 and no longer exists in
python3.

tools/smoketesting/convert-to-tarballs.py
@@ -32,7 +32,7 @@ import signal
 import subprocess
 from ftplib import FTP
 from xml.dom import minidom, Node
-from sgmllib import SGMLParser
+from HTMLParser import HTMLParser
 import requests
 import urlparse
 if not hasattr(__builtins__, 'set'):
@@ -242,9 +242,9 @@ class Options:
                                  node.nodeName + '\n')
                 sys.exit(1)
 
-class urllister(SGMLParser):
+class urllister(HTMLParser):
     def reset(self):
-        SGMLParser.reset(self)
+        HTMLParser.reset(self)
         self.urls = []
 
     def start_a(self, attrs):

commit 35e6722f1efc1a50c57b27603584b7ecd0039e62

Fix
I broke the HTML parser in 62c85310, so now only tarballs available via
SFTP can be downloaded. Update by properly revising our urllister class
to overload an HTMLParser method rather than an SGMLParser method.

Fixes #13

tools/smoketesting/convert-to-tarballs.py
@@ -247,10 +247,11 @@ class urllister(HTMLParser):
         HTMLParser.reset(self)
         self.urls = []
 
-    def start_a(self, attrs):
-        href = [v for k, v in attrs if k=='href']
-        if href:
-            self.urls.extend(href)
+    def handle_starttag(self, tag, attrs):
+        if tag == 'a':
+            href = [v for k, v in attrs if k=='href']
+            if href:
+                self.urls.extend(href)
 
 class TarballLocator:
     def __init__(self, tarballdir, mirrors, local_only=False):